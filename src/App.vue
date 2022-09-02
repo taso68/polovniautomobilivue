4cbfdb338b2574b18dc3392cd5c979e57584df08
Taso <lazar.taso@gmail.com>
2022-09-02 22:36:08 +0200

    Before vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+<div class="app-wrapper">
+  <HeaderTop></HeaderTop>
+  <h1>KURAVVEL</h1>
+</div>
 </template>
+
+<script>
+import HeaderTop from './components/HeaderTop.vue';
+export default {
+  components:{
+    HeaderTop,
+    HeaderTop
+}
+
+}
+</script>
 
 <style>
 #app {
@@ -13,18 +23,19 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  width: 100%;
+  height: 100%;
 }
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+body{
+  margin: 0px;
 }
 </style>
+<style scoped>
+  .app-wrapper{
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background-color: #eee;
+  }
+</style>
+
---
+++ src/components/HeaderTop.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="header-wrapper">
+    <div class="logo-wrapper">
+      <img src="@/assets/camaro-logo.png" alt="">
+    </div>
+    <div class="networks-wrapper">
+      <div class="social-network">
+        <img src="@/assets/facebook-icon.png" alt="">
+      </div>
+      <div class="social-network">
+        <img src="@/assets/insta-icon.png" alt="">
+      </div>
+      <div class="social-network">
+        <img src="@/assets/yt-icon.png" alt="">
+      </div>
+    </div>
+    <div class="wrapper-login">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  .header-wrapper{
+    position: relative;
+    width: 100%;
+    height: 4rem;
+    background-color: black;
+    color:white;
+    display: flex;
+    justify-content: space-around;
+  }
+  .logo-wrapper, .networks-wrapper, .wrapper-login{
+    width: 30%;
+  }
+  .logo-wrapper img{
+    height: 85%;
+    background: transparent;
+  }
+  .logo-wrapper p {
+    margin: 0;
+    font-size: 1rem;
+    font-family: OCR A Std,monospace;
+  }
+  
+  .social-network img{
+    height: 2rem;
+    background: transparent;
+  }
+  .networks-wrapper{
+    display: flex;
+    padding-top: 1rem;
+    justify-content: space-around;
+  }
+
+</style>
